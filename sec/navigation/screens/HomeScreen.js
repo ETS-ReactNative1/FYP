ef303bfa93ead4e7cd315e3e5d512cf5caad6c75
Walter <u3557229@connect.hku.hk>
2022-01-22 22:15:33 +0800

    LOCATION Feature test (with bugssssssssss

--- sec/navigation/screens/HomeScreen.js
+++ sec/navigation/screens/HomeScreen.js
@@ -1,9 +1,57 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import {Location, Permissions} from 'expo';
+import { Component } from 'react/cjs/react.production.min';
 
 export default function HomeScreen({ navigation }) {
+    state={
+        location: {},
+        errorMessage:''
+
+    };
+    componentWillMount=()=>{
+        this._getLocation();
+    }
+
+    _getLocation = async()=>{
+        const {status} = await Permissions.askAsync(permissions.Location);
+
+        if (status !== 'granted'){
+            console.log('No permission of getting location');
+
+            this.setState({
+                errorMessage: 'Permission not granted'
+            })
+        }
+        const userLocation = await Location.getCurrentPositionAsync();
+        this.setState({
+            location: userLocation
+        })
+    }
+    
+    {/*constructor(props){
+        super(props);
+        this.stats = {
+            latitude: 22.3,
+            longitude: 114.1,
+            error: null
+        };
+    }
+    componentDidMount(){
+        navigator.geolocation.getCurrentPosition(position => {
+            this.setState({
+                latitude:position.coords.latitude,
+                longitude: position.coords.longitude,
+                error: null
+            });
+        }, 
+        error => this.setState({error: error.message}),
+        { enableHighAccuracy: true, timeout: 15000, maximumAge: 2000}
+        );
+    }*/}
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             
@@ -16,11 +64,13 @@ export default function HomeScreen({ navigation }) {
                 longitudeDelta: 0.0,
                 }}
             >
+                {/*}
                 <Marker
-                    coordinate={{ latitude : 22.28312 , longitude : 114.13592 }} //change this location if have time
+                    coordinate={this.state.location}
                     title={"Current Location"}
                     description={"Info"}
                 />
+                */}
             </MapView>
     
 
