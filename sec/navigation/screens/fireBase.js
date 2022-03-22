1ccf00f9b5c8fe4bbdf27717c6d33548d74226f4
Walter <u3557229@connect.hku.hk>
2022-03-23 00:06:12 +0800

    testing firebase

---
+++ sec/navigation/screens/fireBase.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import { PermissionsAndroid, Platform, View, Text, SafeAreaView, ScrollView, StyleSheet, Button, FlatList, TextInput} from 'react-native';
+import CallLogs from 'react-native-call-log';
+import notifee, {AndroidGroupAlertBehavior, AndroidImportance} from '@notifee/react-native';
+//init
+import {initializeApp} from 'firebase/app';
+import firestore from '@react-native-firebase/firestore';
+export default class Helper{
+    static async testing(word, session){
+
+    }
+}
+
+
+FirebaseFirestore db = FirebaseFirestore.getInstance();
+//write
+DatabaseReference myRef = database.getReference("message");
+myRef.setValue("Hello, World!");
+//Read from the database
+myRef.addValueEventListener(new ValueEventListener() {
+  @Override
+  public void onDataChange(DataSnapshot dataSnapshot) {
+      // This method is called once with the initial value and again
+      // whenever data at this location is updated.
+      String value = dataSnapshot.getValue(String.class);
+      Log.d(TAG, "Value is: " + value);
+  }
+  @Override
+  public void onCancelled(DatabaseError error) {
+      // Failed to read value
+      Log.w(TAG, "Failed to read value.", error.toException());
+  }
+});
+//end~~~~~~~~~~~~~~~~~~~~
