f7df8c6612e5acbb87ddb1ddacd4a6cc7f52acf8
Raymond Ng <56331289+flyj222@users.noreply.github.com>
2022-04-10 17:33:32 +0800

    Slight updates on details

--- sec/navigation/screens/DetailsScreen.js
+++ sec/navigation/screens/DetailsScreen.js
@@ -7,13 +7,11 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function DetailsScreen({ navigation }) {
     // Global variables
-    var [data, setData] = useState("loading");
-    const [arr, setArr] = useState([]);
     var [listData, setListData] = useState([]);
     const [text, onChangeText] = useState(null);
     const cheerio = require('react-native-cheerio');
 
-    var [record, onChangeText2] = React.useState("");
+    var [record, setRecord] = useState([]);
     var [code, setCode] = useState("");
     var [userCode, getUserCode] = useState("");
 
@@ -31,13 +29,19 @@ export default function DetailsScreen({ navigation }) {
       reference2.set(item);
     };
 
-    function getCallLog() {
-      newRef.on('value', function (snapshot) {
-          setListData(snapshot.val());
+    function getMyCallLog() {
+      reference2.on('value', function (snapshot) {
+          setListData(snapshot.val()); 
       });
     }
 
-    function pushCallLog() {
+    function getCallLog() {
+      newRef.on('value', function (snapshot) {
+          setListData(snapshot.val()); 
+      });
+    }
+
+    async function pushCallLog() {
         addItem(record);
         Alert.alert('Item saved successfully');
     };
@@ -70,17 +74,6 @@ export default function DetailsScreen({ navigation }) {
           } catch (error) {
               // error
           }
-          return Promise.resolve();
-    }
-
-    // daly helper function
-    const delay = ms => new Promise(res => setTimeout(res, ms));
-
-    async function onLoad() {
-        await loadCallLog();
-        await getCode();
-        await delay(2000);
-        pushCallLog();
     }
 
     // Loading Call Log
@@ -97,13 +90,84 @@ export default function DetailsScreen({ navigation }) {
                 }
             ).then(() => {
                 CallLogs.load(10,isDistinct=true).then((c) => setListData(c));
-                CallLogs.load(10,isDistinct=true).then((c) => onChangeText2(c));
+                CallLogs.load(10,isDistinct=true).then((c) => setRecord(c));
             })
         } else {
             console.log("iOS device, no call log available");
         }
     }
 
+    // Web-scraping function for call type identification
+    async function infoScrape(number) {
+      const keywords = ['假冒','騙案','詐騙','自稱']
+
+      // Web Scraping
+      var type = "Safe";
+      //setData("Safe");
+      const response = await fetch('https://hkjunkcall.com/?ft='+number);
+      const text = await response.text();
+      const $ = cheerio.load(text);
+      const res = $('meta[property="og:title"]').attr('content');
+        
+      // Data manipulation & checking
+      const resArr = (res.slice(3)).split(" ");
+      resArr.forEach(element => {
+        keywords.forEach(element2 => {
+          if (element.includes(element2)){
+            //setData("Malicious");
+            type = "Malicious";
+          }
+        });
+      }); 
+      console.log(type);
+      return type;
+    }
+
+    // driver function for automatic call checking
+    async function checkLogNumber() {
+      for (var i=0; i < record.length; i++) {
+        if (!record[i].name) {
+          // Not in call log
+          var data = await infoScrape(record[i].phoneNumber);
+          record[i]["callType"] = data;
+        }
+        else {
+          record[i]["callType"] = "In Contact List";
+        }
+      }
+    }
+
+    // Delivers dummy notification when called
+    async function onDisplayNotification(number, data) {
+      // Create a channel
+      const channelId = await notifee.createChannel({
+        id: 'default',
+        name: 'Default Channel',
+        importance: AndroidImportance.HIGH,
+      });
+  
+      // Display a notification
+      await notifee.displayNotification({
+        title: 'Phone Number Checking',
+        body: 'The phone number '+number+' is found to be '+data+'.',
+        android: {
+          channelId,
+        },
+      });
+    }
+
+    // delay helper function
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
+    async function onLoad() {
+        await loadCallLog();
+        await getCode();
+        await checkLogNumber();
+        await delay(2000);
+        pushCallLog();
+    }
+
+    // Formatting for call log display
     // render format for Call logs
     const ItemView = ({item}) => {
         return (
@@ -119,6 +183,8 @@ export default function DetailsScreen({ navigation }) {
               PhoneNumber : {item.phoneNumber}
               {'\n'}
               Type : {item.type}
+              {'\n'}
+              CallType : {item.callType}
             </Text>
           </View>
         );
@@ -142,61 +208,6 @@ export default function DetailsScreen({ navigation }) {
     useEffect (() => {
         onLoad();
     },[]);
-
-    // Web-scraping function for call type identification
-    async function infoScrape(number) {
-        const keywords = ['假冒','騙案','詐騙','自稱']
-
-        // Web Scraping
-        setData("Safe");
-        const response = await fetch('https://hkjunkcall.com/?ft='+number);
-        const text = await response.text();
-        const $ = cheerio.load(text);
-        const res = $('meta[property="og:title"]').attr('content');
-          
-        // Data manipulation & checking
-        const resArr = (res.slice(3)).split(" ");
-        resArr.forEach(element => {
-          keywords.forEach(element2 => {
-            if (element.includes(element2)){
-              //setData("Malicious");
-              setData("Malicious");
-            }
-          });
-        }); 
-
-    }
-
-    // Delivers dummy notification when called
-    async function onDisplayNotification(number, data) {
-        // Create a channel
-        const channelId = await notifee.createChannel({
-          id: 'default',
-          name: 'Default Channel',
-          importance: AndroidImportance.HIGH,
-        });
-    
-        // Display a notification
-        await notifee.displayNotification({
-          title: 'Phone Number Checking',
-          body: 'The phone number '+number+' is found to be '+data+'.',
-          android: {
-            channelId,
-          },
-        });
-      }
-
-    // driver function for automatic call checking
-    async function checkLogNumber() {
-      for (var i=0; i < listData.length; i++) {
-        if (!listData[i].name) {
-          // Not in call log
-          await infoScrape(listData[i].phoneNumber);
-          listData[i]["callType"] = data;
-          console.log(listData[i]);
-        }
-      }
-    }
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width: '100%', }}>
