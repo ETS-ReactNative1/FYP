dc4101d2079ae4ac9b2bfb6ecaa30fde7ef1205e
Raymond Ng <56331289+flyj222@users.noreply.github.com>
2022-04-12 14:41:49 +0800

    Call log auto upload module done

--- sec/App.js
+++ sec/App.js
@@ -1,14 +1,12 @@
-
 import React, {useEffect, useState} from 'react';
 import MainContainer from './navigation/MainContainer';
 import BackgroundService from 'react-native-background-actions';
-import uploadMap, { downloadLog } from './Functions'
+import uploadMap, { uploadLog, downloadLog } from './Functions'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 function App() {
   BackgroundService.start(veryIntensiveTask, options);
-  // BackgroundService.updateNotification({taskDesc: 'New ExampleTask description'}); // Only Android, iOS will ignore this call
   // iOS will also run everything here in the background until .stop() is called
 
   return (  // navigation>screens
@@ -18,56 +16,36 @@ function App() {
 
 const sleep = (time) => new Promise((resolve) => setTimeout(() => resolve(), time));
 
-    // You can do anything in your task such as network requests, timers and so on,
-    // as long as it doesn't touch UI. Once your task completes (i.e. the promise is resolved),
-    // React Native will go into "paused" mode (unless there are other tasks running,
-    // or there is a foreground app).
-    const veryIntensiveTask = async (taskDataArguments) => {
-        // Example of an infinite loop task
-        const { delay } = taskDataArguments;
-        await new Promise( async (resolve) => {
-            /*
-            var userCode = "";
-            var code = "";
-            if (userCode == "") {
-                //AsyncStorage.getItem('Code').then( (c) => (code = c) );
-                //code = JSON.stringify(AsyncStorage.getItem('Code'));
-                AsyncStorage.getItem('Code', (err, item) => uploadMap(item)  );
-                //code = AsyncStorage.getItem('Code');
-                console.log("35 CODE : ",code);
-            }
-            if (userCode == '') {
-                try {
-                    userCode = AsyncStorage.getItem('Code');
-                }catch(error){}
-            }
-            */
-            for (let i = 0; BackgroundService.isRunning(); i++) {
-                
-                console.log('before',i);
-                
-                AsyncStorage.getItem('Code', (err, item) => uploadMap(item));
-                AsyncStorage.getItem('Code2', (err, item) => downloadLog(item));
-                console.log('after',i);
-                sleep(5000);
-                await sleep(delay);
-            }
-        });
-    };
+// You can do anything in your task such as network requests, timers and so on,
+// as long as it doesn't touch UI. Once your task completes (i.e. the promise is resolved),
+// React Native will go into "paused" mode (unless there are other tasks running,
+// or there is a foreground app).
+const veryIntensiveTask = async (taskDataArguments) => {
+    // Example of an infinite loop task
+    const { delay } = taskDataArguments;
 
-    const options = {
-        taskName: 'Example',
-        taskTitle: 'ExampleTask title',
-        taskDesc: 'ExampleTask description',
-        taskIcon: {
-            name: 'ic_launcher',
-            type: 'mipmap',
-        },
-        color: '#ff00ff',
-        //linkingURI: 'yourSchemeHere://chat/jane', // See Deep Linking for more info
-        parameters: {
-            delay: 5000,
-        },
-    };
+    for (let i = 0; BackgroundService.isRunning(); i++) {
+        console.log('before',i);
+        AsyncStorage.getItem('Code', (err, item) => uploadMap(item));
+        //AsyncStorage.getItem('Code', (err, item) => uploadLog(item));
+        //AsyncStorage.getItem('Code2', (err, item) => downloadLog(item));
+        console.log('after',i);
+        await sleep(delay);
+    }
+};
+
+const options = {
+    taskName: 'Example',
+    taskTitle: 'ExampleTask title',
+    taskDesc: 'ExampleTask description',
+    taskIcon: {
+        name: 'ic_launcher',
+        type: 'mipmap',
+    },
+    color: '#ff00ff',
+    parameters: {
+        delay: 5000,
+    },
+};
 
 export default App;
--- sec/Functions.js
+++ sec/Functions.js
@@ -1,4 +1,3 @@
-
 import React, {useEffect, useState} from 'react';
 import { PermissionsAndroid, Platform, View, Text, SafeAreaView, ScrollView, 
     StyleSheet, Button, FlatList, TextInput, TouchableHighlight, Alert} from 'react-native';
@@ -40,167 +39,84 @@ export default function uploadMap(code) {
     })
 
 }
-/*
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
+const cheerio = require('react-native-cheerio');
 
+// Web-scraping function for call type identification
+async function infoScrape(number) {
+  const keywords = ['假冒','騙案','詐騙','自稱']
 
-export function uploadLogs() {
-    var [listData, setListData] = useState([]);
-    const [text, onChangeText] = useState(null);
-    const cheerio = require('react-native-cheerio');
+  // Web Scraping
+  var type = "Safe";
+  //setData("Safe");
+  const response = await fetch('https://hkjunkcall.com/?ft='+number);
+  const text = await response.text();
+  const $ = cheerio.load(text);
+  const res = $('meta[property="og:title"]').attr('content');
+    
+  // Data manipulation & checking
+  const resArr = (res.slice(3)).split(" ");
+  resArr.forEach(element => {
+    keywords.forEach(element2 => {
+      if (element.includes(element2)){
+        //setData("Malicious");
+        type = "Malicious";
+      }
+    });
+  }); 
+  console.log(type);
+  return type;
+}
 
-    var [record, setRecord] = useState([]);
-    var [code, setCode] = useState("");
-    var [userCode, getUserCode] = useState("");
+// driver function for automatic call checking
+async function checkLogNumber(record) {
+  //return record;
+  for (var i=0; i < record.length; i++) {
+    if (!record[i].name) {
+      // Not in call log
+      var data = await infoScrape(record[i].phoneNumber);
+      record[i]["callType"] = data;
+    }
+    else {
+      record[i]["callType"] = "In Contact List";
+    }
+  }
+  return record;
+}
 
-    var reference2 = firebase
+function pushCallLog(code, record) {
+  var newRef = firebase
       .app()
       .database('https://fyp-project-337408-default-rtdb.asia-southeast1.firebasedatabase.app/')
       .ref('/'+code+'/callRecord');
 
-    var newRef = firebase
-      .app()
-      .database('https://fyp-project-337408-default-rtdb.asia-southeast1.firebasedatabase.app/')
-      .ref('/'+userCode+'/callRecord');
-
-    let addItem = item => {
-      reference2.set(item);
-    };
-
-    function getMyCallLog() {
-      reference2.on('value', function (snapshot) {
-          setListData(snapshot.val()); 
-      });
-    }
-
-    function getCallLog() {
-      newRef.on('value', function (snapshot) {
-          setListData(snapshot.val()); 
-      });
-    }
-
-    async function pushCallLog() {
-        addItem(record);
-        Alert.alert('Item saved successfully');
-    };
-
-    function genCode() {
-      var text = ""
-      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-      
-      for (var i = 0; i < 6; i++)
-          text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-      storeCode(text);
-    }
-
-    async function getCode() {
-      try {
-          var value = await AsyncStorage.getItem('Code');
-          if (value == null) {
-              // No code yet, generate a code
-              genCode();
-          }
-          // Always try to display a code
-          value = await AsyncStorage.getItem('Code');
-          setCode(value);
-          var value2 = await AsyncStorage.getItem('Code2');
-          if (value2 == null) {
-            value2 = value;
-          }
-          getUserCode(value2);  
-          } catch (error) {
-              // error
-          }
-    }
-
-    // Loading Call Log
-    async function loadCallLog() {
-        if (Platform.OS === 'android') {
-            PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.READ_CALL_LOG,
-                {
-                    'title': 'Call Log',
-                    'message': 'Access your call logs',
-                    buttonNeutral: 'Ask Me Later',
-                    buttonNegative: 'Cancel',
-                    buttonPositive: 'OK',
-                }
-            ).then(() => {
-                CallLogs.load(10,isDistinct=true).then((c) => setListData(c));
-                CallLogs.load(10,isDistinct=true).then((c) => setRecord(c));
-            })
-        } else {
-            console.log("iOS device, no call log available");
-        }
-    }
-
-    // Web-scraping function for call type identification
-    async function infoScrape(number) {
-      const keywords = ['假冒','騙案','詐騙','自稱']
-
-      // Web Scraping
-      var type = "Safe";
-      //setData("Safe");
-      const response = await fetch('https://hkjunkcall.com/?ft='+number);
-      const text = await response.text();
-      const $ = cheerio.load(text);
-      const res = $('meta[property="og:title"]').attr('content');
-        
-      // Data manipulation & checking
-      const resArr = (res.slice(3)).split(" ");
-      resArr.forEach(element => {
-        keywords.forEach(element2 => {
-          if (element.includes(element2)){
-            //setData("Malicious");
-            type = "Malicious";
-          }
-        });
-      }); 
-      console.log(type);
-      return type;
-    }
-
-    // driver function for automatic call checking
-    async function checkLogNumber() {
-      for (var i=0; i < record.length; i++) {
-        if (!record[i].name) {
-          // Not in call log
-          var data = await infoScrape(record[i].phoneNumber);
-          record[i]["callType"] = data;
-        }
-        else {
-          record[i]["callType"] = "In Contact List";
-        }
-      }
-    }
-    
-
-    // delay helper function
-    const delay = ms => new Promise(res => setTimeout(res, ms));
-
-    async function onLoad() {
-        await loadCallLog();
-        await getCode();
-        await checkLogNumber();
-        await delay(2000);
-        pushCallLog();
-    }
-
-    
-    // Automatically load call log when the app is launched
-    useEffect (() => {
-        onLoad();
-    },[]);
-
-    return (null);
+  // Push call log
+  newRef.set(record);
 }
 
+export function uploadLog(code) {    
+    // Loading Call Log
+    if (Platform.OS === 'android') {
+      PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.READ_CALL_LOG,
+          {
+              'title': 'Call Log',
+              'message': 'Access your call logs',
+              buttonNeutral: 'Ask Me Later',
+              buttonNegative: 'Cancel',
+              buttonPositive: 'OK',
+          }
+      ).then(() => {
+          CallLogs.load(10,isDistinct=true).then((c) => checkLogNumber(c).then((c2) => pushCallLog(code, c2)));
+      })
+    } else {
+        console.log("iOS device, no call log available");
+    }    
+}
 
-*/
 
 export function downloadLog(code) {
     var listData = [];
